49ed5b7548cd5720178e01b2c47e55f474489c8e
Caue <fernandes.cauep@gmail.com>
2019-11-20 14:08:15 -0300

    - filter and sort db script file names - use full db zip file url

--- handler.py
+++ handler.py
@@ -35,10 +35,10 @@ def download_restore_db(event, context):
     drop_all_tables()
 
     for root, dirs, files in os.walk(DB_SCRIPTS_DIR):
+        files = sorted( (f for f in files if f.endswith(".sql") and not f.startswith('.')))
         for file in files:
-            if file.endswith(".sql") and not file.startswith('.'):
-                file_path = os.path.join(root, file)
-                process_script_file(file_path)
+            file_path = os.path.join(root, file)
+            process_script_file(file_path)
 
     print("Deleting temp files...")
     os.remove(TEMP_ZIP_FILENAME)
@@ -96,11 +96,25 @@ def db_execute_sql(sql, transaction_id):
 def drop_all_tables():
     transaction_id = db_start_transaction()
     
-    table_names = ["content_model_reference", "job_zone_reference", "occupation_data"]
+    table_names = [
+        "content_model_reference", "job_zone_reference", "occupation_data", "scales_reference",
+        "ete_categories", "level_scale_anchors", "occupation_level_metadata", "survey_booklet_locations",
+        "task_categories", "work_context_categories", "abilities", "education_training_experience",
+        "interests", "job_zones", "knowledge", "skills",
+        "task_statements", "task_ratings", "work_activities", "work_context",
+        "work_styles", "work_values", "green_occupations", "green_task_statements",
+        "iwa_reference", "dwa_reference", "tasks_to_dwas", "green_dwa_reference",
+        "tasks_to_green_dwas", "emerging_tasks", "career_changers_matrix", "career_starters_matrix",
+        "unspsc_reference", "tools_and_technology", "alternate_titles", "sample_of_reported_titles",
+        "technology_skills", "tools_used"
+    ]
+    db_execute_sql("SET FOREIGN_KEY_CHECKS = 0;", transaction_id)
+
     for tablename in table_names:
         sql = "DROP TABLE IF EXISTS `" + tablename + "`;"
         db_execute_sql(sql, transaction_id)
 
+    db_execute_sql("SET FOREIGN_KEY_CHECKS = 1;", transaction_id)
     db_commit_transaction(transaction_id)
 
 
